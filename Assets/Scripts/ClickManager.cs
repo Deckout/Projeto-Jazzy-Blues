fed5a0085c105bb20d447f55c68c1cf6e9caf823
Deckout <vinicius.morellisilva@gmail.com>
2024-08-10 18:33:26 -0300

    Update 1
    - UI Adicionado
    - Inicio ClickManager (adicionar +1 ponto ao clicar, spawn da caixa a cada 10 pontos e spawn do Player Character)
    - Adicionar colisão correta para caixa e player

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ClickManager : MonoBehaviour
+{
+    int clicks;
+    TMP_Text contador;
+    Button MainButton;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
